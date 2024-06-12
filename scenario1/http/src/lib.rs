7f6a7b0f2ceaf7d4fdcac591f64692955fdd418a
san <stcr96@gmail.com>
2024-06-13 00:14:06 +0900

    Chapter 2.1 and 2.2

---
+++ scenario1/http/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod httprequest;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
---
+++ scenario1/target/debug/deps/http-f85bbd37293d151b.d
@@ -0,0 +1,6 @@
+/home/san/dev/rust-servers-services-apps/scenario1/target/debug/deps/http-f85bbd37293d151b: http/src/lib.rs http/src/httprequest.rs
+
+/home/san/dev/rust-servers-services-apps/scenario1/target/debug/deps/http-f85bbd37293d151b.d: http/src/lib.rs http/src/httprequest.rs
+
+http/src/lib.rs:
+http/src/httprequest.rs:
