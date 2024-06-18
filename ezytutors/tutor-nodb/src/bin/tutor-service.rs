6a61e3cb18db00477474bab4122531e2f93fd6da
san <stcr96@gmail.com>
2024-06-19 00:03:53 +0900

    Error 3.2.4

--- ezytutors/tutor-nodb/src/bin/tutor-service.rs
+++ ezytutors/tutor-nodb/src/bin/tutor-service.rs
@@ -5,6 +5,9 @@ use std::sync::Mutex;
 #[path = "../handlers.rs"]
 mod handlers;
 
+#[path = "../models.rs"]
+mod models;
+
 #[path = "../routes.rs"]
 mod routes;
 
@@ -19,11 +22,13 @@ async fn main() -> io::Result<()> {
   let shared_data = web::Data::new(AppState {
     health_check_response: "I'm good. You've already asked me ".to_string(),
     visit_count: Mutex::new(0),
+    courses: Mutex::new(vec![]),
   });
   let app = move || {
     App::new()
         .app_data(shared_data.clone())
         .configure(general_routes)
+        .configure(course_routes)
   };
 
   HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
