275d88aef7f1965a8faa2600bf9458893eafa2c5
san <stcr96@gmail.com>
2024-07-04 23:19:06 +0900

    ~Ch10 Finished

--- ch10/async-hello/src/main.rs
+++ ch10/async-hello/src/main.rs
@@ -1,13 +1,45 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+struct AsyncTimer {
+    expiration_time: Instant,
+}
+
+impl Future for AsyncTimer {
+    type Output = String;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.expiration_time {
+            println!("Hello, it's time for Future 1");
+            Poll::Ready(String::from("Future 1 has completed"))
+        } else {
+            println!("Hello, it's not yet time for Future 1. Going to sleep");
+            let waker = cx.waker().clone();
+            let expiration_time = self.expiration_time;
+            std::thread::spawn(move || {
+                let current_time = Instant::now();
+                if current_time < expiration_time {
+                    std::thread::sleep(expiration_time-current_time);
+                }
+                waker.wake();
+            });
+            Poll::Pending
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     println!("Hello before reading file!");
     
     let h1 = tokio::spawn(async {
-        let _file1_contents = read_from_file1().await;
-        println!("{:?}", _file1_contents);
+        let future1 = AsyncTimer {
+            expiration_time: Instant::now() + Duration::from_millis(4000),
+        };
+        println!("{:?}", future1.await);
     });
 
     let h2 = tokio::spawn(async {
@@ -18,14 +50,7 @@ async fn main() {
     let _ = tokio::join!(h1, h2);
 }
 
-async fn read_from_file1() -> String {
-    sleep(Duration::new(4, 0));
-    println!("{:?}", "Processing file 1");
-    String::from("Hello, there from file 1")
-}
-
 async fn read_from_file2() -> String {
     sleep(Duration::new(2, 0));
-    println!("{:?}", "Processing file 2");
-    String::from("Hello, there from file 2")
+    String::from("Future 2 has completed")
 }
